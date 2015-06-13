b374b19e88acf9705a2ed5af1d933389fbc5a83a
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-06-13 23:01:34 +0300

    RocketSave just updates all the tracks and you can export them by pressing
    File -> RemoteExport from Rocket

--- Graffathon/Assets/Scripts/RocketSave.cs
+++ Graffathon/Assets/Scripts/RocketSave.cs
@@ -6,20 +6,26 @@ public class RocketSave : MonoBehaviour {
 	public string trackPath;
 	public string[] trackNames;
 
-	Device device;
-	
-	// Use this for initialization
-	void Start () {
-		device = new Device("", false);
-		device.Connect();
+//	Device device;
+//	
+//	// Use this for initialization
+//	void Start () {
+//		device = new Device("", false);
+//		device.Connect();
+//		foreach (string trackName in trackNames) {
+//			device.GetTrack(trackName);
+//		}
+//		device.Update(0);
+//		device.SaveTracks();
+//	}
+
+	void Update() {		
 		foreach (string trackName in trackNames) {
-			device.GetTrack(trackName);
+			Debug.Log(trackName + " " + Rocket.instance.GetValue(trackName));
 		}
-		device.Update(0);
-		device.SaveTracks();
 	}
 	
-	void OnDestroy() {
-		device.Dispose();
-	}
+//	void OnDestroy() {
+//		device.Dispose();
+//	}
 }
