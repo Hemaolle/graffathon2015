e51c911a05595a2e2af36ba68df355b56ba4a48d
Oskari <oskari.leppaaho@gmail.com>
2015-06-13 13:37:36 +0300

    Removed unused code from Rocket class

--- Graffathon/Assets/Scripts/Rocket.cs
+++ Graffathon/Assets/Scripts/Rocket.cs
@@ -24,11 +24,8 @@ public class Rocket : Singleton<Rocket> {
 	
 	// Update is called once per frame
 	void Update () {
-//		int currentRow;
 		if (playing)
 			row = ((Time.time - startTime) * (bpm / 60f) + startRow);
-//		else 
-//			currentRow = 0;
 		device.Update((int)row);
 		Debug.Log(row);
 	}
@@ -56,39 +53,8 @@ public class Rocket : Singleton<Rocket> {
 			tracks.Add(trackName, device.GetTrack(trackName));
 		}
 		return tracks[trackName].GetValue(row);
-//		if(!playing)
-//			return currentRow;
-//		float nextRow = tracks[trackName].GetValue(row);
-//		return currentRow + (nextRow - currentRow);
-//		return interpolate(currentRow, nextRow, row, tracks[trackName].Type);
 	}
 
-	/**
-     * Interpolates value for row between two TrackKey-objects.
-     *
-     * Interpolation type depends on the first key; only the value is
-     * used from the second key in the process.
-     *
-     * Row can naturally be fractional, so interpolation between
-     * values flowing in time is continuous.
-     */
-//	public static float interpolate(float first, float second, float row, Track.Key.Type type) {
-//		float t = (row - first) / (second - first);
-//			
-//		switch (type) {
-//		case Track.Key.Type.Step:
-//			return first;
-//			//case KeyType.LINEAR:
-//		case Track.Key.Type.Smooth:
-//			t = t * t * (3 - 2*t);
-//			break;
-//		case Track.Key.Type.Ramp:
-//			t = Mathf.Pow(t, 2.0);
-//			break;
-//		}
-//
-//		return first + (second - first) * t;
-//	}
 
 	void OnDestroy() {
 		device.Dispose();
