f7c93cab9d7a5cc76427aa88b126a35795252a69
Oskari <oskari.leppaaho@gmail.com>
2015-06-13 13:49:18 +0300

    A scaling sphere

---
+++ Graffathon/Assets/Scripts/Scale.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scale : MonoBehaviour {
+	public string rocketKey;
+	Vector3 originalScale;
+	// Use this for initialization
+	void Start () {
+		originalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = originalScale * Rocket.instance.GetValue(rocketKey);
+	}
+}
