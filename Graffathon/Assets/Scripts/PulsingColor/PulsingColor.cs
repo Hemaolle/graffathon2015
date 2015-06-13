fa816d746522b063c90bf3f121686d45e9d833ff
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-06-14 00:15:00 +0300

    Renamed camera to targetCamera in PulsingColor to avoid hiding the camera variable

--- Graffathon/Assets/Scripts/PulsingColor/PulsingColor.cs
+++ Graffathon/Assets/Scripts/PulsingColor/PulsingColor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PulsingColor : MonoBehaviour {
 	public Material material;
-	public Camera camera;
+	public Camera targetCamera;
 	public float phase;
 	public Color color1;
 	public Color color2;
@@ -13,7 +13,7 @@ public class PulsingColor : MonoBehaviour {
 		phase = Rocket.instance.GetValue ("color");
 		if (material != null)
 			material.color = Color.Lerp (color1, color2, phase);
-		if (camera != null)
-			camera.backgroundColor = Color.Lerp (color1, color2, phase);
+		if (targetCamera != null)
+			targetCamera.backgroundColor = Color.Lerp (color1, color2, phase);
 	}
 }
