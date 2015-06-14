1a562e2f4b0f4eea96d167cc44fb59363bbe3205
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-06-14 13:25:09 +0300

    Enable credits text with delay

---
+++ Graffathon/Assets/Scripts/EnableRendererWithDelay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableRendererWithDelay : MonoBehaviour {
+
+	public float delay = 10f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (delay);
+		GetComponent<MeshRenderer> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
