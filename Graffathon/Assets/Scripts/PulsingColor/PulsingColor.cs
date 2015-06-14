b5f1b2c3a148201e84b9a865ff3ac477952746a8
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-06-14 13:12:42 +0300

    Fade out, some bloom adjustment

--- Graffathon/Assets/Scripts/PulsingColor/PulsingColor.cs
+++ Graffathon/Assets/Scripts/PulsingColor/PulsingColor.cs
@@ -7,6 +7,9 @@ public class PulsingColor : MonoBehaviour {
 	public float phase;
 	public Color color1;
 	public Color color2;
+	public AudioSource audiosource;
+
+	bool quitCalled;
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,5 +18,17 @@ public class PulsingColor : MonoBehaviour {
 			material.color = Color.Lerp (color1, color2, phase);
 		if (targetCamera != null)
 			targetCamera.backgroundColor = Color.Lerp (color1, color2, phase);
+		if (audiosource != null)
+			audiosource.volume = 1 - phase;
+		if (phase == 1 && !quitCalled) {
+			quitCalled = true;
+			StartCoroutine (QuitApp ());
+		}
+	}
+
+	IEnumerator QuitApp() {
+		yield return new WaitForSeconds (3);
+		Debug.Log ("Quit");
+		Application.Quit ();
 	}
 }
